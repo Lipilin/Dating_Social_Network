10fd6bef4dc748238da459f664fb59dcbddc7f05
Егор Липилин <egorlcrab@gmail.com>
2026-07-22 19:32:47 +0300

    docs: добавлен линтер

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+tseslint.configs.recommended['no-unused-vars'] = 'warn'
+export default tseslint.config(
+   {
+    ignores: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/build/**',
+        '**/.adminjs/**',
+        '*.config.js',
+        '*.config.cjs',
+        'coverage',
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-unused-private-class-members': 'off',
+      'no-console': 'warn',
+      'indent': ['error', 4],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'comma-dangle': ['error', 'never'],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'always'],
+      'space-before-function-paren': ['error', 'never'],
+      'max-len': ['error', { code: 120 }],
+    },
+  }
+)
